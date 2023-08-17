d869ebc13eec497c37c33dbaae0e0e3670ef339b
alansampaio2 <alanmangueira@gmail.com>
2023-08-17 20:37:15 -0300

    Deficiencia Controller

---
+++ SIGENFAB.API/Controllers/DeficienciasController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SIGENFAB.API.Data;
+using SIGENFAB.Shared.Entities;
+
+namespace SIGENFAB.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeficienciasController : ControllerBase
+    {
+        private readonly Contexto _contexto;
+
+        public DeficienciasController(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _contexto.Deficiencias.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Deficiencia deficiencia)
+        {
+            _contexto.Add(deficiencia);
+            await _contexto.SaveChangesAsync();
+            return Ok(deficiencia);
+        }
+    }
+}
