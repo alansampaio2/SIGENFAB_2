1c16b847b7db2d14acfeb02ae33d85b288bfaf38
alansampaio2 <alanmangueira@gmail.com>
2023-08-18 06:42:14 -0300

    Exclusão de Deficiencia via web

---
+++ SIGENFAB.Web/Pages/Deficiencias/DeficienciaEditar.razor.cs
@@ -0,0 +1,57 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using SIGENFAB.Shared.Entities;
+
+namespace SIGENFAB.Web.Pages.Deficiencias
+{
+    public partial class DeficienciaEditar
+    {
+        [Parameter]
+        public int Id { get; set; }
+
+        private Deficiencia? deficiencia;
+        private DeficienciaForm? deficienciaForm;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var responseHTTP = await repositorio.Get<Deficiencia>($"api/deficiencias/{Id}");
+
+            if (responseHTTP.Error)
+            {
+                if (responseHTTP.HttpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    navigationManager.NavigateTo("deficiencias");
+                }
+                else
+                {
+                    var messageError = await responseHTTP.GetErrorMessageAsync();
+                    await sweetAlertService.FireAsync("Error", messageError, SweetAlertIcon.Error);
+                }
+            }
+            else
+            {
+                deficiencia = responseHTTP.Response;
+            }
+        }
+
+        private async Task Edit()
+        {
+            var responseHTTP = await repositorio.Put("api/deficiencias", deficiencia);
+            if (responseHTTP.Error)
+            {
+                var mensajeError = await responseHTTP.GetErrorMessageAsync();
+                await sweetAlertService.FireAsync("Error", mensajeError, SweetAlertIcon.Error);
+            }
+            else
+            {
+                deficienciaForm!.FormPostedSuccessfully = true;
+                navigationManager.NavigateTo("deficiencias");
+            }
+        }
+
+        private void Return()
+        {
+            navigationManager.NavigateTo("deficiencias");
+        }
+    }
+}
--- SIGENFAB.Web/Pages/Deficiencias/DeficienciaIndex.razor.cs
+++ SIGENFAB.Web/Pages/Deficiencias/DeficienciaIndex.razor.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
 using SIGENFAB.Shared.Entities;
 using SIGENFAB.Web.Repositories;
 
@@ -12,8 +13,46 @@ namespace SIGENFAB.Web.Pages.Deficiencias
 
         protected override async Task OnInitializedAsync()
         {
+            await Load();
+        }
+
+        private async Task Load()
+        {
             var responseHttp = await repository.Get<List<Deficiencia>>("api/deficiencias");
             Deficiencias = responseHttp.Response!;
+        }
+
+        private async Task Delete(Deficiencia deficiencia)
+        {
+            var result = await sweetAlertService.FireAsync(new SweetAlertOptions
+            {
+                Title = "Confirmação",
+                Text = "Está certe de que deseja realmente excluir este registro?",
+                Icon = SweetAlertIcon.Question,
+                ShowCancelButton = true
+            });
+            var confirm = string.IsNullOrEmpty(result.Value);
+            if (confirm)
+            {
+                return;
+            }
+            var responseHTTP = await repository.Delete($"api/deficiencias/{deficiencia.Id}");
+            if (responseHTTP.Error)
+            {
+                if (responseHTTP.HttpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    navigationManager.NavigateTo("/");
+                }
+                else
+                {
+                    var mensajeError = await responseHTTP.GetErrorMessageAsync();
+                    await sweetAlertService.FireAsync("Error", mensajeError, SweetAlertIcon.Error);
+                }
+            }
+            else
+            {
+                await Load();
+            }
         }
     }
 }
